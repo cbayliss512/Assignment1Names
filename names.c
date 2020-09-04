418f09e14187f0cec9de1e548bb224d2c7537981
Christopher <cbayliss2@huskers.unl.edu>
2020-09-04 01:37:50 +0000

    Initial commit

---
+++ names.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Christopher Bayliss
+ *          cbayliss2@huskers.unl.edu
+ * Date: 9/3/2020
+ *
+ * CSCE 155H
+ *
+ * This program prints names
+ *
+ **/
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    //Christopher Bayliss
+    printf("Christopher Bayliss\n");
+
+}
